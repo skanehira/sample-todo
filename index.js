767a06dd3bb8ad3c24fc2748776192d24093176f
skanehira <sho19921005@gmail.com>
2020-03-14 19:37:47 +0900

    add pure version

--- index.js
+++ index.js
@@ -1,47 +1,94 @@
-const app = new Vue({
-  el: '#app',
-  data: {
-    form: {},
-    header: [
-      "id",
-      "name",
-      "todo",
-      "operation",
-    ],
-    todos: []
-  },
-  methods: {
-    createTodo() {
-      axios.post("/todos", this.form).then((response) => {
-        alert("登録しました")
-        this.getTodos()
-        this.form = {}
-      }).catch((error) => {
-        console.log(error);
-      })
-    },
-    getTodos() {
-      axios.get("/todos", {}).then((response) => {
-        this.todos = response.data;
-      }).catch((error) => {
-        console.log(error);
-      })
-    },
-    deleteTodo(id) {
-      axios.delete("/todos", {
-        params: {
-          id: id,
-        }
-      }).then((response) => {
-        alert("削除しました")
-        this.getTodos()
-      }).catch((error) => {
-        console.log(error)
-      })
-    }
-  },
-  mounted(){
-    this.getTodos();
-  }
-})
+const table = document.getElementById("table")
 
+function makeTr(data) {
+  const tr = document.createElement("tr")
+
+  for (h of data) {
+    if (h == "button") {
+      const bt = document.createElement("button")
+      bt.textContent = "削除"
+      bt.onclick = function() {
+        const id = data[0]
+
+        fetch(`/todos?id=${id}`, {
+          method: 'DELETE',
+        }).then((response) => {
+          if (response.ok) {
+            alert("削除しました")
+            updateTable()
+          } else {
+            alert("エラー")
+          }
+        }).catch((err) => {
+          console.log(err);
+        })
+      }
+      tr.appendChild(bt)
+    } else {
+      const td = document.createElement("td")
+      td.textContent = h
+      tr.appendChild(td)
+    }
+  }
+
+  return tr
+}
+
+function makeHeader() {
+  const headers = ["id", "name", "todo", "operation"]
+  return makeTr(headers)
+}
+
+function getTodo() {
+  fetch("/todos").then((response) => {
+    return response.json();
+  }).then((todos) => {
+    for (todo of todos) {
+      let t = Object.values(todo)
+      t.push("button")
+      table.appendChild(makeTr(t))
+    }
+  }).catch((err) => {
+    console.log(err);
+  })
+}
+
+function createTodo() {
+  const name = document.getElementById("name")
+  const todo = document.getElementById("todo")
+
+  const form = {
+    name: name.value,
+    todo: todo.value,
+  }
+
+  fetch("/todos", {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(form),
+  }).then(() => {
+    alert("登録しました")
+    updateTable()
+    name.value = ""
+    todo.value = ""
+  }).catch((err) => {
+    console.log(err);
+  })
+
+  console.log(`name: ${name}, todo: ${todo}`)
+}
+
+function deleteTodo() {
+
+}
+
+function updateTable() {
+    table.innerHTML = ""
+    table.appendChild(makeHeader())
+    getTodo()
+}
+
+table.appendChild(makeHeader())
+getTodo()
--- main.go
+++ main.go
@@ -9,39 +9,23 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 )
 
+var db *sql.DB
+
 type Todo struct {
 	ID   int    `json:"id"`
 	Name string `json:"name"`
 	Todo string `json:"todo"`
 }
 
-var db *sql.DB
-
-func init() {
-	var err error
-	db, err = sql.Open("sqlite3", "todo.db")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	sql := `
-create table if not exists todos (id integer primary key autoincrement, name varchar(255), todo varchar(255))
-	`
-
-	if _, err := db.Exec(sql); err != nil {
-		log.Fatal(err)
-	}
-}
-
-func getTodos(w http.ResponseWriter, r *http.Request) {
+func getTodo(w http.ResponseWriter, r *http.Request) {
 	rows, err := db.Query("select * from todos")
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
+		log.Fatal(err)
 	}
 	defer rows.Close()
 
 	todos := []Todo{}
+
 	for rows.Next() {
 		var (
 			id   int
@@ -50,16 +34,15 @@ func getTodos(w http.ResponseWriter, r *http.Request) {
 		)
 
 		if err := rows.Scan(&id, &name, &todo); err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
+			http.Error(w, err.Error(), 500)
 			return
 		}
 
-		t := Todo{id, name, todo}
-		todos = append(todos, t)
+		todos = append(todos, Todo{id, name, todo})
 	}
 
-	if err := json.NewEncoder(w).Encode(todos); err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+	if err := json.NewEncoder(w).Encode(&todos); err != nil {
+		http.Error(w, err.Error(), 500)
 		return
 	}
 }
@@ -67,43 +50,48 @@ func getTodos(w http.ResponseWriter, r *http.Request) {
 func createTodo(w http.ResponseWriter, r *http.Request) {
 	var todo Todo
 	if err := json.NewDecoder(r.Body).Decode(&todo); err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		http.Error(w, err.Error(), 400)
 		return
 	}
 
 	if _, err := db.Exec("insert into todos (name, todo) values (?, ?)", todo.Name, todo.Todo); err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		http.Error(w, err.Error(), 500)
 		return
 	}
 }
 
 func deleteTodo(w http.ResponseWriter, r *http.Request) {
-	ids := r.URL.Query()["id"]
-	if len(ids) == 0 {
-		http.Error(w, "cannot get id parameter", http.StatusInternalServerError)
+	id := r.URL.Query().Get("id")
+	if id == "" {
+		http.Error(w, "no id", 400)
 		return
 	}
-	id := ids[0]
 
 	if _, err := db.Exec("delete from todos where id = ?", id); err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		http.Error(w, err.Error(), 500)
 		return
 	}
 }
 
 func main() {
+	var err error
+	db, err = sql.Open("sqlite3", "todo.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
 	http.Handle("/", http.FileServer(http.Dir(".")))
 	http.HandleFunc("/todos", func(w http.ResponseWriter, r *http.Request) {
 		switch r.Method {
 		case http.MethodGet:
-			getTodos(w, r)
+			getTodo(w, r)
 		case http.MethodPost:
 			createTodo(w, r)
 		case http.MethodDelete:
 			deleteTodo(w, r)
 		}
 	})
-
 	log.Println("start http server :8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
